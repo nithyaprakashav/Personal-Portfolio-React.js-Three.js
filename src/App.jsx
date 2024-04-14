4e0b9ce1b3e231ef28627dc244829f91c319a298
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-14 18:37:49 +0530

    Add Nvabar component and structure App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 const App = ()=> {
   return (
-    <>
-      <div>My 3D portfolio</div>
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary ">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
