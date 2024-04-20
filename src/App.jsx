d7cd8720e57089d5419bfda0aa8712d61c054ded
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-20 18:50:19 +0530

    Add hero component to main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 const App = ()=> {
   return (
@@ -7,8 +8,10 @@ const App = ()=> {
       <div className="relative z-0 bg-primary ">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
+          <Hero/>
         </div>
       </div>
+      
     </BrowserRouter>
   )
 }
