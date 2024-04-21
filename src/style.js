c5f0b2ef4918e643b71707a1291fb5d137f91675
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-21 13:52:11 +0530

    Fix text size

--- src/style.js
+++ src/style.js
@@ -6,7 +6,7 @@ const styles = {
     heroHeadText:
       "font-black text-white lg:text-[40px] sm:text-[30px] xs:text-[25px] text-[40px] lg:leading-[55px] mt-2",
     heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[16px] xs:text-[10px] text-[16px] lg:leading-[35px]",
+      "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[16px] xs:text-[15px] text-[16px] lg:leading-[35px]",
   
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
