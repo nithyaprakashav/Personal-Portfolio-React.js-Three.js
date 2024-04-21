0c7d1b8c95a83a4b337474917e365ac239821408
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-21 22:37:46 +0530

    FIx typo and modify about title

--- src/style.js
+++ src/style.js
@@ -4,9 +4,9 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
     heroHeadText:
-      "font-black text-white lg:text-[40px] sm:text-[30px] xs:text-[25px] text-[40px] lg:leading-[55px] mt-2",
+      "font-black text-white lg:text-[40px] sm:text-[40px] xs:text-[30px] text-[40px] lg:leading-[55px] mt-2",
     heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[16px] xs:text-[15px] text-[16px] lg:leading-[35px]",
+      "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[20px] xs:text-[15px] text-[16px] lg:leading-[35px]",
   
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
