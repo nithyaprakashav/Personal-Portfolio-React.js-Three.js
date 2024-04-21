13be1b4db5be690811f231c4cd51550f73b26a62
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-21 10:05:10 +0530

    Add heading and bio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,19 @@ const Hero = () => {
                     <div className='w-5 h-5 rounded-full bg-[#915eff] ' />
                     <div className='w-1 sm:h-80 h-40 violet-gradient' />
                 </div>
+
+                <div>
+                    <h1 className={`${styles.heroHeadText} text-white`} >Hi , I'm <span className='text-[#915eff]' > Nithya Prakash</span></h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100 `} >
+                        I'm a Full Stack Web Developer, skilled in MERN Stack,<br className='sm:block hidden' /> proficient in Java, JavaScript, and Python.
+                    </p>
+
+                </div>
+
             </div>
+
+            <ComputersCanvas/>
+
         </section>
      );
 }
