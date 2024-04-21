5b74e3ea7e1073456827fb6b5f1258f8ddf265ac
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-21 15:33:27 +0530

    Add About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import About from "./components/About"
+
 
 const App = ()=> {
   return (
@@ -9,9 +11,11 @@ const App = ()=> {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
           <Hero/>
-        </div>
+        </div> 
       </div>
-      
+      <div className="relative z-0 bg-primary ">
+        <About/>
+      </div>
     </BrowserRouter>
   )
 }
