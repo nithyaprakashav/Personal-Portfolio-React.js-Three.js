d8a18abee85bf652a429e2dd1ba6f501d47581ea
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-22 00:38:24 +0530

    Fix NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ const App = ()=> {
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
           <Hero/>
-        </div> 
+        </div>
+        <About/> 
       </div>
-      <div className="relative z-0 bg-primary ">
+      {/* <div className="relative z-0 bg-primary ">
         <About/>
-      </div>
+      </div> */}
     </BrowserRouter>
   )
 }
