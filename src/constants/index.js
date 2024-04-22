f7ca0f9d460ca202156b4c5ab779efc3cf551b94
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-22 19:08:24 +0530

    Edit

--- src/constants/index.js
+++ src/constants/index.js
@@ -105,7 +105,7 @@ import {
       company_name: "SPM Global Technologies",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "October 2023 - April 2024",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
