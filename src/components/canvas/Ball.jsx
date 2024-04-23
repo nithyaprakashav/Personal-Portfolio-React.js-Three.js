2df3e3f6d0aaa71d775f6cc786a6ccec5b9072eb
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-23 12:15:24 +0530

    Add marterial and icons

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,7 +1,7 @@
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React, { Suspense } from 'react'
-import Loader from '../Loader'
+import CanvasLoader from '../Loader'
 
 
 
@@ -12,7 +12,15 @@ const Ball = (props) => {
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2} >
       <ambientLight intensity={0.25} />
-      <directionalLight position={} />
+      <directionalLight position={[0 , 0 ,0.05]} />
+      <mesh castShadow receiveShadow scale={2.75} >
+        <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset polygonOffsetFactor={-5} flatShading
+        />
+        <Decal position={[0,0,1]} map={decal} rotation={[2*Math.PI,0,6.25]} />
+      </mesh>
     </Float>
   )
 }
