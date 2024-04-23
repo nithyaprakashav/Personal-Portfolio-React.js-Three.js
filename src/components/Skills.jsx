e558c4ac67ca5d132f024c07bbc675015824fef0
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-23 11:44:36 +0530

    Design Skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,19 @@
 
+import { technologies } from "../constants";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { BallCanvas } from "./canvas";
+
 
 const Skills = () => {
     return ( 
-        <div>Skills</div>
+        <div className="flex flex-row flex-wrap justify-center gap-10 " >
+            {technologies.map((tech)=>(
+                <div className="w-28 h-28" key={tech.name} >
+                    <BallCanvas icon={tech.icon} />
+                </div>
+            ))}
+        </div>
      );
 }
  
-export default Skills;
+export default SectionWrapper( Skills ,"");
