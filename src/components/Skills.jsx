324f8036ccd45dce7b22f8b3ae962cb730d248ee
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-23 12:29:23 +0530

    Add skillsName

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,8 +8,9 @@ const Skills = () => {
     return ( 
         <div className="flex flex-row flex-wrap justify-center gap-10 " >
             {technologies.map((tech)=>(
-                <div className="w-28 h-28" key={tech.name} >
-                    <BallCanvas icon={tech.icon} />
+                <div className="w-28 h-28 flex flex-col justify-center items-center " key={tech.name} >
+                    <BallCanvas icon={tech.icon}  />
+                    <div className="text-secondary" >{tech.name}</div>
                 </div>
             ))}
         </div>
