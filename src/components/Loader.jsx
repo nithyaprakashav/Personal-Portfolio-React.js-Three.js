33f3fbceeb701bc3ee17f88f8e8fb4e609096538
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-23 15:35:20 +0530

    fix size

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import { Html, useProgress } from "@react-three/drei";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
+
 
 const Loader = () => {
     const {progress} = useProgress()
@@ -9,7 +9,7 @@ const Loader = () => {
             <span className="canvas-load" ></span>
             <p  
                 style={{
-                    fontSize:14,
+                    fontSize:18,
                     color:"#f1f1f1",
                     fontWeight:800,
                     marginTop:40
