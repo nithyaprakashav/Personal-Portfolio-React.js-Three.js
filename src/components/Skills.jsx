582a8f8ae57dd6023fbbbcc589cf0907b78694ac
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-23 14:22:28 +0530

    Add headText and subText to skillsSection

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,19 @@
-
+import { motion } from "framer-motion";
 import { technologies } from "../constants";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { BallCanvas } from "./canvas";
-
+import { textVariant } from "../utils/motion";
+import { styles } from "../style";
 
 const Skills = () => {
     return ( 
-        <div className="flex flex-row flex-wrap justify-center gap-10 " >
+        <>
+        <motion.div variants={textVariant()} >
+            <p className={`${styles.sectionSubText}  `}  >Technologies i have learnt so far</p>
+            <h2 className={`${styles.sectionHeadText}`} >Skillset.</h2>
+        </motion.div>
+        
+        <div className="flex flex-row flex-wrap justify-center gap-10 mt-20" >
             {technologies.map((tech)=>(
                 <div className="w-28 h-28 flex flex-col justify-center items-center " key={tech.name} >
                     <BallCanvas icon={tech.icon}  />
@@ -14,6 +21,7 @@ const Skills = () => {
                 </div>
             ))}
         </div>
+        </>
      );
 }
  
