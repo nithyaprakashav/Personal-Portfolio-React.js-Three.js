bee236eb8ee7c9920d441d380f4ee2807d681fd8
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-24 11:22:08 +0530

    modify certificationsComponent

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,6 +1,29 @@
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import SectionWrapper from "../hoc/SectionWrapper";
+import { fadeIn, textVariant } from "../utils/motion";
+
+
+
+
 const Certifications = () => {
     return ( 
-        
+        <div className="mt-12 bg-black-100 rounded-[20px] " >
+            <div className={`${styles.padding} bg-fuchsia-950 rounded-2xl min-h-[300px] ` } >
+                <motion.div variants={textVariant()} >
+                    <p className={`${styles.sectionSubText}`} >What others say</p>
+                    <h2 className={`${styles.sectionHeadText}`} >Testimonials.</h2>
+                </motion.div>
+            </div>
+            <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7 `} >
+                {testimonials.map((testimonial , index)=>(
+                    <FeedBackCard key={testimonial.name}
+                        index={index}
+                        {...testimonial}
+                    />
+                ))}
+            </div>
+        </div>
      );
 }
  
