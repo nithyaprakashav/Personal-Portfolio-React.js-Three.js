4022a0fbe7a1817f70779c982334a330b5acff8e
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-24 15:46:47 +0530

    Move Starscanvas to Canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { PointMaterial, Points, Preload } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import React, { Suspense, useRef, useState } from 'react'
+import * as random from 'maath/random/dist/maath-random.esm'
 
-const Stars = () => {
+
+const Stars = (props) => {
   return (
     <div>Stars</div>
   )
 }
 
-export default Stars
+const StarsCanvas = () => {
+  return (
+    <div className='w-full h-full absolute inset-0 z-[-1] ' >
+        <Canvas
+          camera={{position:[0,0,1]}}
+        >
+            <Suspense fallback={null} >
+              <Stars/>
+            </Suspense>
+
+            <Preload all />
+
+        </Canvas>
+    </div>
+  )
+}
+
+
+export default StarsCanvas
