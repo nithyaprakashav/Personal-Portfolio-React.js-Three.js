70eccf9b0114964e9d671c74cdd28c2139351391
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-25 01:58:32 +0530

    Add Socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,7 @@ import { styles } from "../style";
 import { EarthCanvas } from "./canvas";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { slideIn } from "../utils/motion";
+import Socials from "./Socials";
 
 
 
@@ -26,7 +27,7 @@ const Contact = () => {
     return ( 
         <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden " >
             <motion.div variants={slideIn("left","tween",0.2 , 1)}
-                className="flex-[0.75] bg-black-100 p-8 rounded-2xl "
+                className="flex-[1] bg-black-100 p-8 rounded-2xl  "
             >
                 <p className={styles.sectionSubText} >Get in touch</p>
                 <h3 className={styles.sectionHeadText} >Contact.</h3>
@@ -77,11 +78,14 @@ const Contact = () => {
 
             </motion.div>
 
-            <motion.div variants={slideIn("right","tween",0.2 , 1)}
-                className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px] "
-            >
-                <EarthCanvas/>
-            </motion.div>
+            
+                <motion.div
+                    variants={slideIn("right", "tween", 0.2, 1)}
+                    className="xl:h-full md:h-[550px] h-[350px] bg-black-100 p-8 rounded-2xl"
+                >
+                    <Socials />
+                </motion.div>
+            
 
         </div>
      );
