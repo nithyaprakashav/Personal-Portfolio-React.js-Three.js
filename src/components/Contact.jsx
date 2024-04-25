d6b0c65decd287241cb3d69e06f8641c2e8ba0e8
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-25 14:56:53 +0530

    fix contact design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
     return ( 
         <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden " >
             <motion.div variants={slideIn("left","tween",0.2 , 1)}
-                className="flex-[1] bg-black-100 p-8 rounded-2xl  "
+                className="flex-[1.4] bg-black-100 p-8 rounded-2xl  "
             >
                 <p className={styles.sectionSubText} >Get in touch</p>
                 <h3 className={styles.sectionHeadText} >Contact.</h3>
@@ -79,13 +79,13 @@ const Contact = () => {
             </motion.div>
 
             
-                <motion.div
-                    variants={slideIn("right", "tween", 0.2, 1)}
-                    className="xl:h-full md:h-[550px] h-[350px] bg-black-100 p-8 rounded-2xl"
-                >
-                    <Socials />
-                </motion.div>
-            
+            <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+            >
+                <Socials/>
+            </motion.div>
+                    
 
         </div>
      );
