9ae9a6530ab8acbea31b632b0e289af46197a2c4
Nithya Prakash A V <nithyaprakashav22@gmail.com>
2024-04-28 03:16:18 +0530

    add download resume feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { styles } from "../style";
 import { navLinks } from "../constants";
 import { logo, menu , close } from "../assets";
 import MyPicture from "../assets/MyPicture.jpg"
-
+import { MdOutlineFileDownload } from "react-icons/md";
 
 
 const Navbar = () => {
@@ -17,6 +17,9 @@ const Navbar = () => {
             className={`${styles.paddingX} w-full flex items-center py-5 fixed  top-0 z-20 bg-primary `}
         >
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto" >
+                <div className="flex items-center gap-4  " >
+
+                
                 <Link to={"/"} className="flex items-center gap-2" onClick={()=>{
                     setActive(" ")
                     window.scrollTo(0,0)
@@ -25,6 +28,18 @@ const Navbar = () => {
                     <img src={MyPicture}  alt="logo" className="w-9 h-9 object-contain rounded-full" />
                     <p className="text-white text-[18px] font-bold cursor-pointer " > Nithya Prakash A V</p>
                 </Link>
+
+                <div className="flex items-center gap-2" onClick={()=>{
+                    window.open("https://drive.google.com/file/d/1Z1JbJPuwPvdhPArz9aC5aDO2mXtTdW5R/view?usp=sharing")
+                }} >
+                    <div className="border-white border bg-tertiary rounded-lg px-3 py-1 flex items-center cursor-pointer">
+                        <MdOutlineFileDownload className="w-5 h-5 mr-2 fill-current text-white" />
+                        <span className="text-secondary text-[14px] ">My Resume</span>
+                    </div>
+                </div>
+
+                </div>
+
                 <ul className="list-none hidden sm:flex flex-row gap-10 " >
                     {navLinks.map((link)=>(
                         <li
